e60bd040859e4498b556715ab86fcfec8ec9e827
Alan Vaudagna <alanvaudagna@gmail.com>
2017-09-07 00:49:34 -0300

    Colocando responsabilidades en la parte correcta del modelo.

--- app/src/main/java/com/tallerv/pizzeria/MainActivity.java
+++ app/src/main/java/com/tallerv/pizzeria/MainActivity.java
@@ -2,11 +2,8 @@ package com.tallerv.pizzeria;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -14,37 +11,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Instancio una nueva Pizza con los datos que cada una especifica y uso un metodo que me
-        //permite generar una vista de pizza en funcion de los datos de una que le pase por parametro.
-        View viewMuzza = parsePizza(new Pizza("Muzzarela", R.drawable.muzzarella, 10));
-        View viewNapo = parsePizza(new Pizza("Napolitana", R.drawable.napolitana, 15));
-        View viewJamon = parsePizza(new Pizza("Jamon y queso", R.drawable.jamon, 20));
-
-        //TODO: El metodo parsePizza esta bueno, pero no se si es el mejor lugar para ponerlo aca...
-        //TODO: Pensemos, el Activity, ¿deberia ser responsable de saber "inflar" un objeto Pizza a su representacion como View?
-        //TODO: De solo formularse la pregunta ya suena que esto es algo que estaria mejor modelado en otro lado...
+        //Asi queda con el refactor de parsePizza implementado como metodo de la clase Pizza.
+        View viewMuzza = new Pizza(getBaseContext(), "Muzzarela", R.drawable.muzzarella, 10).parsePizza();
+        View viewNapo = new Pizza(getBaseContext(), "Napolitana", R.drawable.napolitana, 15).parsePizza();
+        View viewJamon = new Pizza(getBaseContext(), "Jamon y queso", R.drawable.jamon, 20).parsePizza();
 
         LinearLayout pizzaLl = (LinearLayout) findViewById(R.id.pizza_container);
         pizzaLl.addView(viewMuzza);
         pizzaLl.addView(viewNapo);
         pizzaLl.addView(viewJamon);
-    }
-
-
-    /**
-     * Recibe la representacion de una pizza y devuelve la vista con sus datos.
-     * @param pizza la pizza a representar en la vista
-     * @return la vista con los datos de la pizza
-     */
-    private View parsePizza(Pizza pizza) {
-        //Se "infla" el template del item de pizza.
-        View pizzaItem = LayoutInflater.from(getBaseContext()).inflate(R.layout.pizza_item, null, false);
-        //Se castea lo devuelto por findViewById al objeto visual que sabemos que es realmente y lo configuramos con la data de la pizza.
-        ((TextView) pizzaItem.findViewById(R.id.nombre)).setText(pizza.getNombre()); //Para el TextView nos interesa setear el texto
-        ((ImageView) pizzaItem.findViewById(R.id.foto)).setImageResource(pizza.getFotoId()); //Para el ImageView el resourceId de la imagen (resourceId porque tenemos guardada la imagen en el proyecto, sino esto seria diferente)
-        ((TextView) pizzaItem.findViewById(R.id.precio)).setText(String.valueOf(pizza.getPrecio()));
-        //Le agregamos el listener de OnClick a la vista.
-        pizzaItem.setOnClickListener(new PizzaOnClickListener(getBaseContext(), pizza.getNombre()));
-        return pizzaItem;
     }
 }
--- app/src/main/java/com/tallerv/pizzeria/Pizza.java
+++ app/src/main/java/com/tallerv/pizzeria/Pizza.java
@@ -1,9 +1,16 @@
 package com.tallerv.pizzeria;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 /**
  * Clase modelo de la Pizza.
  */
 public class Pizza {
+    private Context context;
     private String nombre;
     private int fotoId;
     private double precio;
@@ -20,6 +27,18 @@ public class Pizza {
         this.precio = precio;
     }
 
+    /**
+     * Modelo de Pizza agregando Context para instanciar View representativa.
+     * @param context contexto
+     * @param nombre nombre
+     * @param fotoId id de recurso para la foto
+     * @param precio costo actual
+     */
+    public Pizza(Context context, String nombre, int fotoId, double precio) {
+        this(nombre, fotoId, precio);
+        this.context = context;
+    }
+
     public double getPrecio() {
         return precio;
     }
@@ -32,6 +51,10 @@ public class Pizza {
         return nombre;
     }
 
+    public Context getContext() {
+        return context;
+    }
+
     public void setFotoId(int fotoId) {
         this.fotoId = fotoId;
     }
@@ -42,6 +65,25 @@ public class Pizza {
 
     public void setPrecio(double precio) {
         this.precio = precio;
+    }
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+
+    /**
+     * @return la vista representativa a la pizza
+     */
+    //Notar como ahora no necesitamos pasarle parametros al metodo (usa informacion propia de la instancia actual del objeto)
+    //La informacion que usa tiene visibilidad en toda la clase (porque esta como atributo o como metodo de clase).
+    public View parsePizza() {
+        View pizzaItem = LayoutInflater.from(context).inflate(R.layout.pizza_item, null, false);
+        //Ahora toda la informacion de la pizza la accedemos directo por los geters.
+        ((TextView) pizzaItem.findViewById(R.id.nombre)).setText(getNombre());
+        ((ImageView) pizzaItem.findViewById(R.id.foto)).setImageResource(getFotoId());
+        ((TextView) pizzaItem.findViewById(R.id.precio)).setText(String.valueOf(getPrecio()));
+        pizzaItem.setOnClickListener(new PizzaOnClickListener(context, getNombre()));
+        return pizzaItem;
     }
 }
 
