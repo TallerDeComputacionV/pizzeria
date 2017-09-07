2db793d8d683d53772de3b6b09bdd7da1744c435
Alan Vaudagna <alanvaudagna@gmail.com>
2017-09-06 23:28:41 -0300

    Agregacion de vistas (Views) programaticamente.

--- app/src/main/java/com/tallerv/pizzeria/MainActivity.java
+++ app/src/main/java/com/tallerv/pizzeria/MainActivity.java
@@ -2,17 +2,51 @@ package com.tallerv.pizzeria;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
-//MainActivity de nuestra App, es la primera que mostraremos. Hereda de AppCompatActivity funcionalidad base.
 public class MainActivity extends AppCompatActivity {
-    //Se sobreescribe el metodo definido por la clase padre (AppCompatActivity)
     @Override
-    //El metodo onCreate se llama en el proceso de inicializacion del Activity.
-    //Usualmente es donde se "infla" el layout que representa su interfaz grafica.
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState); //Para extender el comportamiento ya implementado en el onCreate de AppCompatActivity
-        setContentView(R.layout.activity_main); //"Infla" el layout principal del activity
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        //TODO: Este es el MainActivity mas basico que se puede tener.
+        LayoutInflater layoutInflater = LayoutInflater.from(getBaseContext()); //Objeto que "infla" layouts
+        View viewMuzzarella = layoutInflater.inflate(R.layout.muzzarella_item, null, false); //"inflado" de layouts, devuelve un objeto del tipo View
+        View viewNapolitana = layoutInflater.inflate(R.layout.napolitana_item, null, false);
+        View viewJamon = layoutInflater.inflate(R.layout.jamon_item, null, false);
+
+        //Desde los activities al hacer findViewById se busca el layout con el id especificado en el que se uso para setContentView
+        LinearLayout pizzaLl = (LinearLayout) findViewById(R.id.pizza_container);
+        pizzaLl.addView(viewMuzzarella); //Se agrega el objeto View pasado como parametro a la lista de elementos del LinearLayout
+        pizzaLl.addView(viewNapolitana);
+        pizzaLl.addView(viewJamon);
+
+        //Se setea el onClickListener para el objeto View.
+        //El OnClickListener define comportamiento para el evento onClick que se efectue sobre la vista (View).
+        //Ejemplo definiendo el OnClickListener como clase anonima (inner class)
+        viewMuzzarella.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getBaseContext(), "Muzzarella", Toast.LENGTH_SHORT).show();
+            }
+        });
+        viewNapolitana.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getBaseContext(), "Napolitana", Toast.LENGTH_SHORT).show();
+            }
+        });
+        viewJamon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getBaseContext(), "Jamon y Muzzarella", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //TODO: Aca podemos ver que los OnClickListeners definidos son muy similares (indicio de repeticion de logica). Lo unico que cambia es el
+        //TODO: texto del mensaje que muestran. ¿Como podriamos extraer esa logica para reutilizarla?
     }
 }
